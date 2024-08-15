566c887ba206f8e9aa7f8bfb5c4bda6ae404ec0e
NicholasBalkins <103457226+NicholasBalkins@users.noreply.github.com>
2024-08-15 04:31:56 +0000

    teste3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,54 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_V = 28;
+const int LED_PIN_V = 4;
+const int BTN_PIN_G = 26;
+const int LED_PIN_G = 6;
+
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN_V);
+  gpio_set_dir(BTN_PIN_V, GPIO_IN);
+  gpio_pull_up(BTN_PIN_V);
+
+  gpio_init(LED_PIN_V);
+  gpio_set_dir(LED_PIN_V, GPIO_OUT);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
   while (true) {
+    if (!gpio_get(BTN_PIN_V)) {
+      int status = gpio_get(LED_PIN_V);
+      if (status == 0) {
+        gpio_put(LED_PIN_V, 1);
+      }
+      if (status == 1) {
+        gpio_put(LED_PIN_V, 0);
+      }
+      while (!gpio_get(BTN_PIN_V)) {
+      };
+    }
+    if (!gpio_get(BTN_PIN_G)) {
+      int status = gpio_get(LED_PIN_G);
+      if (status == 0) {
+        gpio_put(LED_PIN_G, 1);
+      }
+      if (status == 1) {
+        gpio_put(LED_PIN_G, 0);
+      }
+      while (!gpio_get(BTN_PIN_G)) {
+      };
+    }
+
+
+
   }
 }
